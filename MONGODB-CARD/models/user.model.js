5788a367d569ecb40b1b683539c8aa463bc2165b
lakhani12 <hitlakhani3@gmail.com>
2026-04-06 19:42:04 +0530

    feat: initialize MongoDB card application with Express and EJS
    - Added package.json for project dependencies and scripts
    - Created server.js to set up Express server and routes for creating and reading cards
    - Implemented views for displaying cards and a form for card creation using EJS
    - Integrated Tailwind CSS for styling the application

---
+++ MONGODB-CARD/models/user.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/user-card');
+
+let userSchema = new mongoose.Schema({
+    fullname: String,
+    username: String,
+    email: String,
+    Image: String,
+},
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('usercard', userSchema);
